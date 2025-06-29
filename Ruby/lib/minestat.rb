a62a711deb763ba4dbba8bace21c03937f4d92a2
SeongHoon Ryu <4997174+ryush00@users.noreply.github.com>
2025-06-29 21:26:49 +0900

    fix: integer can't use empty?

--- Ruby/lib/minestat.rb
+++ Ruby/lib/minestat.rb
@@ -640,7 +640,7 @@ class MineStat
         strip_motd()
         @current_players = json_data['players']['online'].to_i
         @max_players = json_data['players']['max'].to_i
-        if !@protocol.empty?
+        if @protocol != 0
           @online = true
         else
           retval = Retval::UNKNOWN
